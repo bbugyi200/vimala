dce4f61b04d002ec997922ce80aeec6fe121f608
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-02-05 21:48:25 -0500

    Add vim() function

--- src/vimala/__init__.py
+++ src/vimala/__init__.py
@@ -2,10 +2,10 @@
 
 import logging as _logging
 
-from ._core import dummy
+from ._vim import vim
 
 
-__all__ = ["dummy"]
+__all__ = ["vim"]
 
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
---
+++ src/vimala/_vim.py
@@ -0,0 +1,26 @@
+"""The vimala package's catch-all module.
+
+You should only add code to this module when you are unable to find ANY other
+module to add it to.
+"""
+
+from __future__ import annotations
+
+import subprocess as sp
+from typing import Iterable
+
+
+def vim(
+    *args: str, lineno: int = None, commands: Iterable[str] = ()
+) -> sp.Popen:
+    """Execute `vim`.."""
+    extra_args: list[str] = []
+    if lineno is not None:
+        extra_args.append(f"+{lineno}")
+
+    for cmd in commands:
+        extra_args.extend(["-c", cmd])
+
+    cmd_args = ["vim", *args, *extra_args]
+    popen = sp.Popen(cmd_args)
+    return popen
