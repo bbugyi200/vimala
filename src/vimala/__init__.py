3e6f42a771ba5242f0d4f1b2f87c0a14bff9c9c1
Bryan Bugyi <bryanbugyi34@gmail.com>
2022-02-07 19:29:48 -0500

    Bump version: 0.1.0 → 0.1.1

--- src/vimala/__init__.py
+++ src/vimala/__init__.py
@@ -9,6 +9,6 @@ __all__ = ["vim"]
 
 __author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 _logging.getLogger(__name__).addHandler(_logging.NullHandler())
